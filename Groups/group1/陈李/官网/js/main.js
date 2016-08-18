df65286163214835de77cc573eda74f2594575e2
chenli <605988809@qq.com>
2016-08-18 23:14:13 +0800

    carousel

--- Groups/group1/陈李/官网/js/main.js
+++ Groups/group1/陈李/官网/js/main.js
@@ -1,21 +1,122 @@
-//首屏 和我聊聊
-var $chat = $('.chat img'),
-	chatImg = $chat.attr('data-img'),
-	chatGif = $chat.attr('data-gif')
-$chat.on('mouseover', function(){
-	$chat.attr('src', chatGif)
-})
-$chat.on('mouseout', function(){
-	$chat.attr('src', chatImg)
+//hover更换图片
+function hoverChange($node){
+	this.imgSrc = $node.attr('data-img')
+	this.gifSrc = $node.attr('data-gif')
+	this.bind($node)
+}
+
+hoverChange.prototype.bind = function($node){
+	var _this = this
+	$node.on('mouseover', function(){
+		$node.attr('src', _this.gifSrc)
+	})
+	$node.on('mouseout', function(){
+		$node.attr('src', _this.imgSrc)
+	})
+}
+
+var $talk = $('.chat img')
+var $qq = $('.contact img')
+new hoverChange($talk)
+$qq.each(function(){
+	new hoverChange($(this))
 })
 
 //carousel
-var $crsCt = $('.mod-carousel ul'),
-	$items = $crsCt.find('li'),
-	itemsWidth = $items.width(),
-	itemsCount = $items.length
+function Carousel($node){
+    this.imgCt = $node.find('ul')
+    this.item = this.imgCt.find('li')
+    this.itemWidth = this.item.outerWidth(true)
+    this.itemCount = this.item.length
+    this.pre = $node.find('.tri-right')
+    this.next = $node.find('.tri-left')
+    this.bullet = $node.find('.bullet')
 
-$crsCt.css({
-	width: itemsWidth* itemsCount,
-	left: 0
-})
+    this.imgCt.css({
+        width: this.itemCount* this.itemWidth
+    })
+
+    this.curIdx = 0
+    this.isAnimate = false
+
+    this.init()
+}
+
+Carousel.prototype = {
+    bind: function(){
+        var _this = this
+        _this.pre.on('click',function(){
+            _this.doPre()
+        })
+        _this.next.on('click',function(){
+            _this.doNext()
+        })
+        _this.bullet.find('li').on('click',function(){
+            var idx = $(this).index()
+            if(idx<_this.curIdx){
+                _this.doPre(_this.curIdx- idx)
+            }
+            else{
+                _this.doNext(idx- _this.curIdx)
+            }
+        })
+    },
+
+    doPre: function(idx){
+        var _this = this
+        idx = idx || 1
+        if(this.curIdx === 0){
+        	return
+        }
+        if(!_this.isAnimate){
+            _this.isAnimate = true
+            _this.doScaleLeft()
+            _this.imgCt.animate({
+                left: '+=' + _this.itemWidth* idx
+            },function(){
+                _this.curIdx--
+                _this.setBullet()
+                _this.isAnimate = false
+            })
+        }
+    },
+
+    doNext: function(idx){
+        var _this = this
+        idx = idx || 1
+        if(this.curIdx === 3){
+        	return
+        }
+        if(!_this.isAnimate){
+            _this.isAnimate = true
+            _this.doScaleRight()
+            _this.imgCt.animate({
+                left: '-=' + _this.itemWidth* idx
+            },function(){
+                _this.curIdx++
+                _this.setBullet()
+                _this.isAnimate = false
+            })
+        }
+    },
+
+    doScaleRight: function(){
+    	this.item.removeClass('active').eq(this.curIdx+2).addClass('active')
+    },
+
+    doScaleLeft: function(){
+    	this.item.removeClass('active').eq(this.curIdx).addClass('active')
+    },
+
+    setBullet: function(){
+        this.bullet.children().removeClass('active').eq(this.curIdx).addClass('active') 
+   },
+
+    init: function(){
+        this.bind()
+    }
+
+}
+
+var $carousel1 = $('.mod-carousel')
+new Carousel($carousel1)
