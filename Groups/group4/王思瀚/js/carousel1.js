643b69c2d895208a6cf5a5bc4db6bf0816b7eca4
envyandgreed <825011977@qq.com>
2016-08-26 20:43:32 +0800

    'add'

--- Groups/group4/王思瀚/js/carousel1.js
+++ Groups/group4/王思瀚/js/carousel1.js
@@ -1,3 +1,39 @@
-/**
- * Created by envy on 2016/8/24.
- */
+function carousel1($cur) {
+    this.$cur = $cur;
+    this.$ul = $cur.find('.carousel>ul');
+    this.$li = $cur.find('.list');
+    this.$imgCount = this.$li.size();
+    this.$li.css({
+            'width':this.$cur.outerWidth() / 3,
+            'height':this.$cur.outerWidth() /3
+    });
+    this.$ul.css({
+            'width':this.$imgCount * this.$li.outerWidth(true) + 5,
+            'height':this.$li.outerHeight(true)
+    })
+
+    this.$pre = $cur.find('.pre');
+    this.$next = $curfind('.next');
+    this.isAnimate = true;
+    this.num = 1;
+    this.bind();
+}
+
+carousel1.prototype = {
+    bind: function(){
+            var me = this;
+            me.$next.on('click',function(){
+                me.toNext()
+            })
+            me.$pre.on('click',function(){
+                me.toPre()
+            });
+    },
+    toNext: function(){
+        var me = this;
+        if(me.isAnimate){
+            me.isAnimate = false;
+
+        }
+    }
+}
