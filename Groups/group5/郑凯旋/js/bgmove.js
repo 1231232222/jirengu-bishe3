5af56ddf83131443d27e4dc43f0589b742ddbc7c
souldestroyin <souldestroyin@gmail.com>
2016-08-31 12:29:08 +0800

    初版完成

--- Groups/group5/郑凯旋/js/bgmove.js
+++ Groups/group5/郑凯旋/js/bgmove.js
@@ -1,17 +1,19 @@
-var $mountains=$('.mountains');
-var winH=$(window).height(),
-    winW=$(window).width(),
-    $mountains=$('.mountain');
+function bgmove($node,dx,dy){
+    var winW= $(window).width()/2,
+        winH= $(window).height()/2;
+    this.$ct=$node.parent();
 
-function resetBg(x,y){
-    x=x||winW/2;
-    y=y||winH/2;
-    $mountains.each(function(i,value){
-        $this=$(this);
-        $this.css({
-            left:,
-            top:
-        });
+    this.$ct.on('mousemove',function(e){
+       	$node.css({
+			'transform': 'translate('+ (e.pageX-winW)/dx + 'px,'+ (e.pageY-winH)/dy +'px)',
+		});
     })
 }
-resetBg();
+
+var bg3 = new bgmove($('.mountain3'),30,40),
+    bg2 = new bgmove($('.mountain2'),50,60),
+    bg1 = new bgmove($('.mountain1'),70,80);
+
+var introbg1= new bgmove($('.intro-bg1'),70,80),
+    introbg2= new bgmove($('.intro-bg2'),30,40);
+
